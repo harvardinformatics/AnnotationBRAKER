c3913bf1d6e3c3ba7bc9598b041a1aadb35fbd76
Adam Freedman <adamfreedman@fas.harvard.edu>
2025-02-25 13:00:20 -0500

    added braker runner slurm script

---
+++ braker_snakemake_slurm_runner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J brakersnake
+#SBATCH -n 1                 
+#SBATCH -t 72:00:00        
+#SBATCH -p # add partition      
+#SBATCH --mem=2000           
+#SBATCH -o logs/test.%A.out # need to create logs directory first  
+#SBATCH -e logs/test.%A.err  
+
+module purge
+module load python
+mamba activate snakemake_py311 # need to have created a snakemake conda environment
+
+snakemake --snakefile workflow/Snakefile --use-conda --profile ~/.config/snakemake/AnnotationBRAKER --workflow-profile ./profiles/slurm
+
+
+
