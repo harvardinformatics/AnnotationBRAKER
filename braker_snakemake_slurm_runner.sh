46e860e40cae365d1afbecbd9f711dc4bb8a800e
Adam Freedman <adamfreedman@fas.harvard.edu>
2025-03-05 14:29:32 -0500

    changes to use use singularity directive and auto pulldown of sif

--- braker_snakemake_slurm_runner.sh
+++ braker_snakemake_slurm_runner.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1                 
 #SBATCH -t 72:00:00        
 #SBATCH -p sapphire # add partition      
-#SBATCH --mem=2000           
+#SBATCH --mem=10000           
 #SBATCH -o logs/test.%A.out # need to create logs directory first  
 #SBATCH -e logs/test.%A.err  
 
@@ -11,7 +11,7 @@ module purge
 module load python
 mamba activate snakemake_py311 # need to have created a snakemake conda environment
 
-snakemake --snakefile workflow/Snakefile --use-conda --profile ~/.config/snakemake/AnnotationBRAKER --workflow-profile ./profiles/slurm
+snakemake --snakefile workflow/Snakefile --use-conda  --profile ~/.config/snakemake/AnnotationBRAKER --workflow-profile ./profiles/slurm
 
 
 
